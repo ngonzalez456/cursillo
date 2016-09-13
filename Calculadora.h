e801b6da84af9aa1b6ad62ff59cc2acee12d2c6d
ngonzalez456 <josefegosa@gmail.com>
2016-09-13 14:58:34 -0600

    Create Calculadora.h

---
+++ Calculadora.h
@@ -0,0 +1,47 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+template <typename data>
+class Calculadora {
+    
+private:
+    int N;
+    AnyType* D;
+
+public:
+
+    SuperArray() {
+        N = 0;
+        D = 0;
+    }
+
+    SuperArray(int c) {
+        N = c;
+        D = new AnyType[N];
+    }
+
+    virtual ~SuperArray() {
+        delete[] D;
+    }
+
+    void Borrar() {
+        delete[] D;
+        N = 0;
+        D = 0;
+    }
+
+    AnyType operator[](int i) {
+        if (i >= 0 && i < N)
+            return D[i];
+    }
+
+    int largo() {
+        return N;
+    }
+
+};
+
+
+
+
+#endif /* CALCULADORA_H */
