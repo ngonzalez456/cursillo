c10088774c0c22ddea41f508a1006e57adc06d85
Isaac Edo. Gómez <isaac.gomez@ucr.ac.cr>
2016-09-16 15:29:34 -0600

    Matriz.cpp con sobrecarga de operadores

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,32 +1,62 @@
-#include "Figuras.h"
+#include "Calculadora.h"
+#include "Matriz.h"
 
-Matriz::Matriz() {};
 
-Matriz::Martriz(string nombre, string color) {
-    this->nombre = nombre;
-    this->color = color;
-};
+Matriz::Matriz() {}
 
-Matriz::Matriz(const Figuras& orig){};
+Matriz::Matriz(double arreglo) {
+    this->arreglo=arreglo;    
+}
 
-Matriz::~Matriz(){};
+Matriz::Matriz(const Matriz& orig){}
 
-void Matriz::operator+(){
+Matriz::~Matriz(){}
+
+void Matriz::operator+(double arreglo2, Matriz c){
+    for(int i; i<=7; i++){
+        for(int j; j<=7; j++){
+            c[i][j]=this->arreglo[i][j] + arreglo2[i][j];
+        }
+    }
+}
+
+void Matriz::operator-(double arreglo2, Matriz c){
+    
+    for(int i; i<=7; i++){
+        for(int j; j<=7; j++){
+            c[i][j]= this->arreglo[i][j] - arreglo2[i][j];
+        }
+    }
+}
+
+void Matriz::operator*(double arreglo2, Matriz c){
+    
+    for(int i; i<=7; i++){
+        for(int j; j<=7; j++){
+            for(int k; k<=7; k++)
+            c[i][j]= this->arreglo[i][k] += arreglo2[k][j];
+        }
+    }
+}
+
+void Matriz::operator/(Matriz c){
+    while (sizeof(this->arreglo2)/4 != sizeof(c.arreglo)){
+        cout << "Introduzca una matriz cuadrada";
+    }    
+        for(int i; i<=7; i++){
+            for(int j; j<=7; j++){
+                c[i][j]= this->arreglo[i][j] / arreglo2[i][j];
+            }
+        }
+    
     
 }
 
-void Matriz::operator-(){
-    
-}
-
-void Matriz::operator*(){
-    
-}
-
-void Matriz::operator/(){
-    
-}
-
-void Matriz::operator~(){
-    
+void Matriz::operator~(Matriz c){
+    for(int i; i<=7; i++){
+        cout << endl;
+        for(int j; j<=7; j++){
+            cout << c[i][j];
+        }
+    }
 }
