c9a056c635aa11659f7f4c43b2975dd816d8dc0a
Isaac Edo. Gómez <isaac.gomez@ucr.ac.cr>
2016-09-16 15:25:59 -0600

    Calculadora.h con headers y librerias

--- Calculadora.h
+++ Calculadora.h
@@ -1,47 +1,51 @@
 #ifndef CALCULADORA_H
 #define CALCULADORA_H
 
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <iostream>
+
+#include "Matriz.h"
+
+using namespace std;
+
 template <typename data>
 
 class Calculadora {
     
 public:
+    
+    Calculadora();
+    Calculadora();
+    Calculadora(const Calculadora& orig);
+    virtual ~Calculadora();
 
     void add(data &d1, const data &d2){
 
         return (d1 + d2);
 
-    };
+    }
 
     void sub(data &d1, const data &d2){
 
         return (d1 - d2);
 
-    };
+    }
 
     void mul(data &d1, const data &d2){
 
         return (d1 * d2);
 
-    };
+    }
 
     void div(data &d1, const data &d2){
 
         return (d1 / d2);
 
-    };
-
-    void print(data &d){
-
-    };
-
-    Calculadora() {
-        
     }
 
-    virtual ~Calculadora() {
-
-    }
+    void print(data &d);
 
 };
 
